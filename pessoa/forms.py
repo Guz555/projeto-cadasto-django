37eae6039722d2269a69ceb3d429c1dd226ca0a2
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-08-15 17:42:54 -0300

    Criado o CRUD da aplicação

---
+++ pessoa/forms.py
@@ -0,0 +1,16 @@
+from dataclasses import field
+from pyexpat import model
+from django import forms
+from .models import Pessoa
+
+
+class PessoaForm(forms.ModelForm):
+    data_nascimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type": "date"}
+        )
+    )
+
+    class Meta:
+        model = Pessoa
+        fields = ['nome_completo', 'data_nascimento', 'ativa']
---
+++ pessoa/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Pessoa(models.Model):
+    nome_completo = models.CharField(max_length=256)
+    ativa = models.BooleanField(default=True)
+    data_nascimento = models.DateField(null=True)
+
+    def __str__(self) -> str:
+        return self.nome_completo
