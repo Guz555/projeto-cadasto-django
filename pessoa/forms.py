d19ee8f1411ea14876c03bb1f5019ef158c3f01e
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-08-16 11:58:46 -0300

    Relacionando models

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -1,16 +1,22 @@
 from dataclasses import field
 from pyexpat import model
 from django import forms
-from .models import Pessoa
+from .models import Pessoa, Contato
 
 
 class PessoaForm(forms.ModelForm):
-    data_nascimento = forms.DateField(
+    """data_nascimento = forms.DateField(
         widget=forms.TextInput(
             attrs={"type": "date"}
         )
-    )
+    )"""
 
     class Meta:
         model = Pessoa
         fields = ['nome_completo', 'data_nascimento', 'ativa']
+
+
+class ContatoForm(forms.ModelForm):
+    class Meta:
+        model = Contato
+        fields = ['nome', 'email', 'telefone']
