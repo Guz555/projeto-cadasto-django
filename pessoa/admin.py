669f8ffe6efe4cf2513cdb4dd7bcc69e0cf3bebc
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-08-18 12:33:20 -0300

    adicionado login_required

--- pessoa/admin.py
+++ pessoa/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Pessoa, Contato
 
-# Register your models here.
+
+@admin.action(description='Ativar selecionados')
+def ativar_todos(modeladmin, request, queryset):
+    queryset.update(ativa=True)
+
+
+@admin.action(description='Desativar selecionados')
+def desativar_todos(modeladmin, request, queryset):
+    queryset.update(ativa=False)
+
+
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome_completo',
+        'data_nascimento',
+        'ativa'
+    ]
+    list_filter = [
+        'ativa',
+        'data_nascimento'
+    ]
+    search_fields = [
+        'nome_completo'
+    ]
+
+    actions = [
+        ativar_todos,
+        desativar_todos
+    ]
+
+
+admin.site.register(Pessoa, PessoaAdmin)
+admin.site.register(Contato)
